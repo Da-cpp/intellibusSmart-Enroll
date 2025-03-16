9681b7d67cc9da59941700d2015957a5169633fe
Da-cpp <charles.denee@gmail.com>
2025-03-16 07:57:17 -0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,23 @@
-export default [
-  {
-    ignores: ["node_modules", "dist"], // Ignore unnecessary folders
+{
+  "$schema": "https://json.schemastore.org/eslintrc",
+  "root": true,
+  "extends": ["next", "next/core-web-vitals", "eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  "parser": "@typescript-eslint/parser",
+  "plugins": ["@typescript-eslint", "prettier"],
+  "rules": {
+    "prettier/prettier": "warn",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    "no-console": "warn",
+    "no-debugger": "warn",
+    "react/no-unescaped-entities": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
-  {
-    rules: {
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"],
-    },
-  },
-];
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
+}
